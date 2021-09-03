365c7d7237c00e6caf092ce2ae4ead6ffbb9bcdf
dammonmarrod <68920289+dammonmarrod@users.noreply.github.com>
2021-09-03 06:11:10 +0200

    Create intercambioExamenes.js

---
+++ paginaWeb_Oposiciones/scripts/intercambioExamenes.js
@@ -0,0 +1,91 @@
+/*PARA INTERCAMBIAR LOS EXAMENES*/
+
+function cambiarDiv_primero() {
+  let container_1 = document.getElementById("primero_id");
+  container_1.style.display = "block";
+  let container_2 = document.getElementById("segundo_id");
+  container_2.style.display = "none";
+  let container_3 = document.getElementById("tercero_id");
+  container_3.style.display = "none";
+  let container_4 = document.getElementById("cuarto_id");
+  container_4.style.display = "none";
+  let container_5 = document.getElementById("quinto_id");
+  container_5.style.display = "none";
+  let container_6 = document.getElementById("sexto_id");
+  container_6.style.display = "none";
+}
+
+function cambiarDiv_segundo() {
+  let container_1 = document.getElementById("primero_id");
+  container_1.style.display = "none";
+  let container_2 = document.getElementById("segundo_id");
+  container_2.style.display = "block";
+  let container_3 = document.getElementById("tercero_id");
+  container_3.style.display = "none";
+  let container_4 = document.getElementById("cuarto_id");
+  container_4.style.display = "none";
+  let container_5 = document.getElementById("quinto_id");
+  container_5.style.display = "none";
+  let container_6 = document.getElementById("sexto_id");
+  container_6.style.display = "none";
+}
+
+function cambiarDiv_tercero() {
+  let container_1 = document.getElementById("primero_id");
+  container_1.style.display = "none";
+  let container_2 = document.getElementById("segundo_id");
+  container_2.style.display = "none";
+  let container_3 = document.getElementById("tercero_id");
+  container_3.style.display = "block";
+  let container_4 = document.getElementById("cuarto_id");
+  container_4.style.display = "none";
+  let container_5 = document.getElementById("quinto_id");
+  container_5.style.display = "none";
+  let container_6 = document.getElementById("sexto_id");
+  container_6.style.display = "none";
+}
+
+function cambiarDiv_cuarto() {
+  let container_1 = document.getElementById("primero_id");
+  container_1.style.display = "none";
+  let container_2 = document.getElementById("segundo_id");
+  container_2.style.display = "none";
+  let container_3 = document.getElementById("tercero_id");
+  container_3.style.display = "none";
+  let container_4 = document.getElementById("cuarto_id");
+  container_4.style.display = "block";
+  let container_5 = document.getElementById("quinto_id");
+  container_5.style.display = "none";
+  let container_6 = document.getElementById("sexto_id");
+  container_6.style.display = "none";
+}
+
+function cambiarDiv_quinto() {
+  let container_1 = document.getElementById("primero_id");
+  container_1.style.display = "none";
+  let container_2 = document.getElementById("segundo_id");
+  container_2.style.display = "none";
+  let container_3 = document.getElementById("tercero_id");
+  container_3.style.display = "none";
+  let container_4 = document.getElementById("cuarto_id");
+  container_4.style.display = "none";
+  let container_5 = document.getElementById("quinto_id");
+  container_5.style.display = "block";
+  let container_6 = document.getElementById("sexto_id");
+  container_6.style.display = "none";
+}
+
+function cambiarDiv_sexto() {
+  let container_1 = document.getElementById("primero_id");
+  container_1.style.display = "none";
+  let container_2 = document.getElementById("segundo_id");
+  container_2.style.display = "none";
+  let container_3 = document.getElementById("tercero_id");
+  container_3.style.display = "none";
+  let container_4 = document.getElementById("cuarto_id");
+  container_4.style.display = "none";
+  let container_5 = document.getElementById("quinto_id");
+  container_5.style.display = "none";
+  let container_6 = document.getElementById("sexto_id");
+  container_6.style.display = "block";
+}
